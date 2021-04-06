6e77a443218501bbe5f57a9b7d9edce947816c13
Benis Tambe <will.jenifer@yahoo.com>
2021-04-05 22:35:29 -0400

    commit tree 2886317b976371f34b669c4c09ac00b24d7793f8

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,35 @@
 class Actor < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :shows, through: :characters
+
+  def full_name
+    "#{self.first_name} #{self.last_name}"
+  end
+
+  def list_roles
+    characters.map do |character|
+        "#{character.name} - #{character.show.name}"
+      end
+  end
 end
+
+=begin
+
+Show 
+Network
+Character
+
+Network has many shows
+Network has many chanracters through shows
+
+Show belongs to Network
+Show has many characters
+
+Character blongs to show
+character belongs to network via shows
+
+An actor has many characters and has many shows through characters.
+A character belongs to an actor and belongs to a show.
+A show has many characters and has many actors through characters.
+
+=end
